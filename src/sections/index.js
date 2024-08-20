fc9e53268671a27ccc3c0ed71fe902e324cb20c4
Saad <imran.saad50@gmail.com>
2024-08-20 15:18:35 +0530

    Completed Hero Sections and added skeleton files

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import PopularProducts from "./PopularProducts";
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
