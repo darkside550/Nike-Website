e622e5d00e0a0159c49f519866042a339f7702cd
Saad <imran.saad50@gmail.com>
2024-08-21 11:22:20 +0530

    Completed Offers and reviews section

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,38 @@
+import { Button } from "../components";
+import { offer } from "../assets/images";
+import { arrowRight } from "../assets/icons";
+
 const SpecialOffer = () => {
-  return <div>SpecialOffer</div>;
+  return (
+    <section className='flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container '>
+      <div className='flex-1'>
+        <img src={offer} alt='gs' height={687} width={773} className='object-contain w-full' />
+      </div>
+
+      <div className='flex flex-1 flex-col'>
+        <h2 className='font-palanquin capitalize text-4xl lg:max-w-lg font-bold'>
+          <span className='text-coral-red'> Special </span> Offer
+        </h2>
+        <p className='mt-4 info-text'>
+          Embark on a shopping journey that redefines your experience with unbeatable deals. From
+          premier selections to incredible savings, we offer unparalleled value that sets us apart.
+        </p>
+        <p className='mt-6 info-text'>
+          Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the
+          loftiest expectations. Your journey with us is nothing short of exceptional.
+        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop now' iconURL={arrowRight} />
+          <Button
+            label='Learn more'
+            backgroundColor='bg-white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SpecialOffer;
